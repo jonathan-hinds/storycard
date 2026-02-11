83a7fa4c5a4bdee887ad2b53944254c115447795
Jonathan Hinds <32693919+jonathan-hinds@users.noreply.github.com>
2026-02-11 00:27:49 -0500

    Fix phase manager matchmaking button module import

--- public/card-game/index.js
+++ public/card-game/index.js
@@ -1,2 +1,3 @@
 export { CardGameClient } from './core/CardGameClient.js';
+export { CARD_ZONE_TYPES, DEFAULT_ZONE_FRAMEWORK } from './core/zoneFramework.js';
 export { SINGLE_CARD_TEMPLATE } from './templates/singleCardTemplate.js';
